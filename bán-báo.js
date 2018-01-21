20b7589478ceb8567585dd63871184014812930f
AsUs <hien.le98dn@gmail.com>
2018-01-21 09:51:32 +0700

    init project

---
+++ bán-báo.js
@@ -0,0 +1,5 @@
+/**
+ * Created by AsUs on 1/16/2018.
+ */
+
+
---
+++ js/my-JQ.js
@@ -0,0 +1,46 @@
+// $(document).ready(function () {
+//     var menu = $('#nav1');
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() >= 95) {
+//             if (!menu.hasClass('navbar-fixed-top')) {
+//                 menu.addClass('navbar-fixed-top', 0, "easeOutBounce");
+//             }
+//         }
+//         else {
+//             if (menu.hasClass('navbar-fixed-top')) {
+//                 menu.removeClass('navbar-fixed-top', 0, "easeOutBounce");
+//             }
+//         }
+//     });
+// });
+// $(window).bind('scroll', function () {
+//     if ($(window).scrollTop() > 50) {
+//         $('.menu').addClass('fixed');
+//     } else {
+//         $('.menu').removeClass('fixed');
+//     }
+// });
+
+// $(document).ready(function(){
+//     $(window).bind('scroll', function() {
+//         var navHeight = $( window ).height() - 70;
+//         if ($(window).scrollTop() > navHeight) {
+//             $('nav').addClass('f-nav');
+//         }
+//         else {
+//             $('nav').removeClass('f-nav');
+//         }
+//     });
+// });
+
+// $("document").ready(function($){
+//     var nav = $('#nav1');
+//
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 0) {
+//             nav.addClass("f-nav");
+//         } else {
+//             nav.removeClass("f-nav");
+//         }
+//     });
+// });
